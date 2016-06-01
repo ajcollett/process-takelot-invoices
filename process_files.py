f58ac2d713b980b978de2b786b7b6125d952b5ad
Andrew James Collett <ajcollett.sun@gmail.com>
2016-06-01 20:04:01 +0200

    We added some more stuff

--- process_files.py
+++ process_files.py
@@ -1,9 +1,12 @@
+"""A large script to do a simple job, enter invoices into Manager."""
 import pyManager as pm
 
 __author__ = 'Andrew James Collett'
+log_file = open('log.txt', 'w')
 
 
 def is_num(ref):
+    """Check if the ref is a number."""
     try:
         int(ref)
         return True
@@ -12,33 +15,50 @@ def is_num(ref):
 
 
 def get_manager_info(host, user, business):
+    """Get managers info, including static info and lists of info."""
+    static_info = dict()
 
     manager_object = pm.manager_object(host, user, business)
     customers = manager_object.get_customers()
     sales_invoices = manager_object.get_sales_invoices()
-    tracking_codes = manager_object.get_tracking_codes()
-    bank_accounts = manager_object.get_cash_accounts()
-    inventory_items = manager_object.get_inventory()
-    accounts = manager_object.get_profit_loss_accounts()
+    sales_items = manager_object.get_sales_inventory_items()
 
-    # emails = list()
-    # refs = list()
-    #
-    # for customer in customers:
-    #     emails.append(customer['Email'])
-    #
-    # for invoice in invoices:
-    #     refs.append(invoice['Reference'])
-    #
-    # return emails, refs
+    track_codes = manager_object.get_tracking_codes()
+    for code_ref in track_codes:
+        if 'takealot sales' in track_codes[code_ref]['Name']:
+            static_info['tracking_code'] = code_ref
+
+    log_file.write('We got tracking code: ' + static_info['tracking_code'] +
+                   '\n')
+
+    bank_acc = manager_object.get_cash_accounts()
+    for account_ref in bank_acc:
+        if ('takealot seller account' in bank_acc[account_ref]['Name']):
+            static_info['bank_account'] = account_ref
+
+    log_file.write('We got bank account: ' + static_info['bank_account'] +
+                   '\n')
+
+    accounts = manager_object.get_profit_loss_accounts()
+    for prof_los_account_ref in accounts:
+        if ('takealot Success fee' in accounts[prof_los_account_ref]['Name']):
+            static_info['success_fee'] = prof_los_account_ref
+        if ('takealot Handling fee' in accounts[prof_los_account_ref]['Name']):
+            static_info['handling_fee'] = prof_los_account_ref
+
+    log_file.write('We got success and handling fee: ' +
+                   static_info['success_fee'] +
+                   ' ' + static_info['handling_fee'] + '\n')
+
+    return(manager_object, customers, sales_invoices, sales_items, static_info)
 
 
 def create_invoices_dict(invoices, OrderID, Date, Title, Qty, Price, Email,
                          m_emails):
-
+    """Create the Invoices dict and add details to it."""
     if is_num(OrderID):
         if Email in m_emails:
-            print Email + ' is in Manager already'
+            log_file.write(Email + ' is in Manager already')
         else:
             try:
                 invoices[OrderID]['items'].append([Title, Qty])
@@ -53,12 +73,12 @@ def create_invoices_dict(invoices, OrderID, Date, Title, Qty, Price, Email,
 
 def add_to_dict(invoices, T_Dt, T_Tp, T_ID, T_Des, Cus,
                 Ref_Tp, Ref, Ex_VAT, VAT, In_VAT, Bal, m_refs):
-
+    """Add the details to the Invoices dict."""
     try:
         ref = Ref.split()[2].replace(')', '').strip()
         if is_num(ref):
             if ref in m_refs:
-                print ref + ' is in Manager already'
+                log_file.write(ref + ' is in Manager already')
             else:
                 try:
                     invoices[str(ref)]['name'] = Cus
@@ -67,13 +87,14 @@ def add_to_dict(invoices, T_Dt, T_Tp, T_ID, T_Des, Cus,
                         'date': T_Dt, 'type': T_Tp, 'description': T_Des,
                         'amount': In_VAT}
                 except (KeyError) as e:
-                    print "This key is not in the dictionary", e
+                    log_file.write("This key is not in the dictionary", e)
 
     except IndexError as e:
         return
 
 
 def get_data(invoice_file, statement_file, m_emails, m_refs):
+    """Get the data out of files, and add to dict, if not already there."""
     invoices = {}
     [create_invoices_dict(invoices, OrderID, Date, Title, Qty, Price, Email,
                           m_emails)
@@ -92,25 +113,34 @@ def get_data(invoice_file, statement_file, m_emails, m_refs):
 def __main__():
     user = 'api'
     host = 'https://manager.procsum.co.za'
-    # invoice_file = open('invoice.csv', 'r')
-    # statement_file = open('statement.csv', 'r')
-    # inv_proc = open('process.csv', 'w')
-    # customers = open('customers.csv', 'w')
+    invoice_file = open('invoice.csv', 'r')
+    statement_file = open('statement.csv', 'r')
+    m_o, customers, invoices, items, static_info = get_manager_info(
+        host, user, 'ProcSum')
 
-    # m_emails, m_refs = get_manager_info(host, user, 'ProcSum')
-    get_manager_info(host, user, 'ProcSum')
+    m_emails = list()
+    m_refs = list()
 
-    # invoices = get_data(invoice_file, statement_file, m_emails, m_refs)
+    try:
+        for customer in customers:
+            m_emails.append(customers[customer]['Email'])
 
-    # keys = invoices.keys()
-    # keys.sort()
-    # keys.reverse()
+        for invoice in invoices:
+            m_refs.append(invoices[invoice]['Reference'])
+    except (KeyError) as e:
+        log_file.write("This key is not in the dictionary", e)
 
-    # customers.write('Name	Email' + '\n')
+    # TODO check all the things
+    # (self, issue_date, ref, to, lines)
 
-    # for key in keys:
-    #     write_files(invoices[key], inv_proc, customers)
+    for ref in invoices:
+        m_o.post_customer(name=invoices[ref]['name'],
+                          email=invoices[ref]['email'])
 
+    final_customers = m_o.get_customers()
+
+    # for ref in invoices:
+    #     man.post_sales_invoice(issue_date=order['date'], ref=ref, to=)
 
 if __name__ == "__main__":
     __main__()
