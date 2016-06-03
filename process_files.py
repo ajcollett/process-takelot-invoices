c05f283ec86cf639df61a63be3dc008b9fac03bb
Andrew James Collett <ajcollett.sun@gmail.com>
2016-06-03 12:26:21 +0200

    Added a few changes

--- process_files.py
+++ process_files.py
@@ -119,8 +119,8 @@ def __main__():
     host = 'https://manager.procsum.co.za'
     invoice_file = open('invoice.csv', 'r')
     statement_file = open('statement.csv', 'r')
-#    m_o, customers, invoices, items, static_info = get_manager_info(
-#        host, user, 'ProcSum')
+    m_o, customers, invoices, items, static_info = get_manager_info(
+       host, user, 'ProcSum')
 
     m_emails = list()
     m_refs = list()
@@ -149,14 +149,101 @@ def __main__():
 
     print('Posted all the things.\n')
     final_customers = m_o.get_customers()
+    final_customers = {v: k for k, v in final_customers.items()}
 
-    for ref in invoices:
-        man.post_sales_invoice(issue_date=order['date'], ref=ref, to=)
+    # items = {v: k for k, v in items.items()}
+
+    sales_items = {'Flirc Raspberry Pi Case': {
+                   'Description': 'The most beautifully crafted Raspberry Pi' +
+                   ' Case. An aluminium body with a built in heat sink, our' +
+                   ' case is the perfect home for your Raspberry Pi B+ and' +
+                   ' will become another one of your favourite media centre' +
+                   ' companions.',
+                   'Account': 'ea44f579-9548-4954-baf0-48538aceff1e',
+                   'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
+                   'InventoryItem': '85cd0eb9-d4c3-491d-9639-e20eab51e5e1',
+                   'Item': 'a0184658-6ad3-4a87-bfdd-002e74c6384e'
+                   }, 'Flirc USB': {
+                   'Description': 'Flirc USB IR reciever PC remote',
+                   'AccountID': 'ea44f579-9548-4954-baf0-48538aceff1e',
+                   'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
+                   'InventoryItem': '77b6e2d4-4f19-4e06-9c31-f6c39cb6592a',
+                   'Item': '19816a8e-0344-4e7f-88b2-42704d3cc99d'
+                   }, 'Raspberry Pi 3 Model B (parallel import)': {
+                   'Description': 'Features & Benefits of the Pi 3 Broadcom' +
+                   ' BCM2837 chipset running at 1.2 GHz 64-bit quad-core ARM' +
+                   ' Cortex-A53 802.11 b/g/n Wireless LAN Bluetooth 4.1' +
+                   ' (Classic & Low Energy) Dual core Videocore IV' +
+                   ' Multimedia co-processor 1 GB LPDDR2 memory Supports all' +
+                   ' the latest ARM GNU/Linux distributions and Windows 10 ' +
+                   'IoT microUSB connector for 2.5 A power supply 1 x 10/100' +
+                   ' Ethernet port 1 x HDMI video/audio connector 1 x RCA' +
+                   ' video/audio connector 1 x CSI camera connector 4 x USB' +
+                   ' 2.0 ports 40 GPIO pins Chip antenna DSI display' +
+                   ' connector microSD card slot Dimensions: 85 x 56 x 17 mm',
+                   'AccountID': '8ac854ab-eee5-43de-8790-54f42bc74bd0',
+                   'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
+                   'InventoryItem': 'df8539b8-104b-413c-bbf7-651dc857bf50',
+                   'Item': 'b6b223a3-cc0e-43d3-8817-3631f5f0e391'
+                   }
+                   }
+
+
+    for order in invoices:
+        transs = invoices[order]['transactions']
+        line = []
+        cnt = 0
+        for key in transs:
+            trans = transs[key]
+            line[cnt] = trans
+            cnt = cnt + 1
+        m_o.post_sales_invoice(issue_date=order['date'], ref=ref,
+                               to=final_customers[order['email']],
+                               lines=)
 
 if __name__ == "__main__":
     __main__()
 
 """
+  "Lines": [
+    {
+      "Description": "The most beautifully crafted Raspberry Pi Case. An aluminium body with a built in heat sink, our case is the perfect home for your Raspberry Pi B+ and will become another one of your favourite media centre companions.",
+      "Account": "ea44f579-9548-4954-baf0-48538aceff1e",
+      "Item": "a0184658-6ad3-4a87-bfdd-002e74c6384e",
+      "Amount": 349.0,
+      "InventoryItem": "85cd0eb9-d4c3-491d-9639-e20eab51e5e1",
+      "TrackingCode": "d590095a-fe46-4a3e-8989-3c9564d69d4e"
+    }
+  ],
+
+{/api/4e437d1d-a396-4deb-82fb-264fd8db9103/a0184658-6ad3-4a87-bfdd-002e74c6384e.json
+  "Name": "Flirc Case takealot",
+  "Description": "The most beautifully crafted Raspberry Pi Case. An aluminium body with a built in heat sink, our case is the perfect home for your Raspberry Pi B+ and will become another one of your favourite media centre companions.",
+  "UnitPrice": 349.0,
+  "AccountID": "ea44f579-9548-4954-baf0-48538aceff1e",
+  "TrackingCode": "d590095a-fe46-4a3e-8989-3c9564d69d4e",
+  "InventoryItem": "85cd0eb9-d4c3-491d-9639-e20eab51e5e1"
+}
+
+
+{/api/4e437d1d-a396-4deb-82fb-264fd8db9103/19816a8e-0344-4e7f-88b2-42704d3cc99d.json
+  "Name": "Flirc USB takealot",
+  "Description": "Flirc USB IR reciever PC remote",
+  "UnitPrice": 399.0,
+  "AccountID": "ea44f579-9548-4954-baf0-48538aceff1e",
+  "TrackingCode": "d590095a-fe46-4a3e-8989-3c9564d69d4e",
+  "InventoryItem": "77b6e2d4-4f19-4e06-9c31-f6c39cb6592a"
+}
+
+
+{/api/4e437d1d-a396-4deb-82fb-264fd8db9103/b6b223a3-cc0e-43d3-8817-3631f5f0e391.json
+  "Name": "Raspberry Pi 3",
+  "Description": "Features & Benefits of the Pi 3\r\nâ€¢Broadcom BCM2837 chipset running at 1.2 GHz\r\nâ€¢64-bit quad-core ARM Cortex-A53\r\nâ€¢802.11 b/g/n Wireless LAN\r\nâ€¢Bluetooth 4.1 (Classic & Low Energy)\r\nâ€¢Dual core Videocore IVÂ® Multimedia co-processor\r\nâ€¢1 GB LPDDR2 memory\r\nâ€¢Supports all the latest ARM GNU/Linux distributions and Windows 10 IoT\r\nâ€¢microUSB connector for 2.5 A power supply\r\nâ€¢1 x 10/100 Ethernet port\r\nâ€¢1 x HDMI video/audio connector\r\nâ€¢1 x RCA video/audio connector\r\nâ€¢1 x CSI camera connector\r\nâ€¢4 x USB 2.0 ports\r\nâ€¢40 GPIO pins\r\nâ€¢Chip antenna\r\nâ€¢DSI display connector\r\nâ€¢microSD card slot\r\nâ€¢Dimensions: 85 x 56 x 17 mm",
+  "UnitPrice": 999.0,
+  "AccountID": "8ac854ab-eee5-43de-8790-54f42bc74bd0",
+  "TrackingCode": "d590095a-fe46-4a3e-8989-3c9564d69d4e"
+}
+
 def write_files(order, inv_proc, customers):
 
     transs = order['transactions']
