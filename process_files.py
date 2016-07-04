5ad08ef630d29f49251a7d98667b35f079d0b9c7
Andrew James Collett <ajcollett.sun@gmail.com>
2016-07-04 22:44:10 +0200

    Some work

--- process_files.py
+++ process_files.py
@@ -8,46 +8,7 @@ import pprint
 __author__ = 'Andrew James Collett'
 log_file = open('log.txt', 'w')
 
-sales_items = {'Flirc Raspberry Pi Case': {
-               'Description': 'The most beautifully crafted Raspberry Pi' +
-               ' Case. An aluminium body with a built in heat sink, our' +
-               ' case is the perfect home for your Raspberry Pi B+ and' +
-               ' will become another one of your favourite media centre' +
-               ' companions.',
-               'Account': 'ea44f579-9548-4954-baf0-48538aceff1e',
-               'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
-               'InventoryItem': '85cd0eb9-d4c3-491d-9639-e20eab51e5e1',
-               'Item': 'a0184658-6ad3-4a87-bfdd-002e74c6384e'
-               }, 'Flirc USB': {
-               'Description': 'Flirc USB IR reciever PC remote',
-               'Account': 'ea44f579-9548-4954-baf0-48538aceff1e',
-               'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
-               'InventoryItem': '77b6e2d4-4f19-4e06-9c31-f6c39cb6592a',
-               'Item': '19816a8e-0344-4e7f-88b2-42704d3cc99d'
-               }, 'Raspberry Pi 3 Model B (parallel import)': {
-               'Description': 'Features & Benefits of the Pi 3 Broadcom' +
-               ' BCM2837 chipset running at 1.2 GHz 64-bit quad-core ARM' +
-               ' Cortex-A53 802.11 b/g/n Wireless LAN Bluetooth 4.1' +
-               ' (Classic & Low Energy) Dual core Videocore IV' +
-               ' Multimedia co-processor 1 GB LPDDR2 memory Supports all' +
-               ' the latest ARM GNU/Linux distributions and Windows 10 ' +
-               'IoT microUSB connector for 2.5 A power supply 1 x 10/100' +
-               ' Ethernet port 1 x HDMI video/audio connector 1 x RCA' +
-               ' video/audio connector 1 x CSI camera connector 4 x USB' +
-               ' 2.0 ports 40 GPIO pins Chip antenna DSI display' +
-               ' connector microSD card slot Dimensions: 85 x 56 x 17 mm',
-               'Account': '8ac854ab-eee5-43de-8790-54f42bc74bd0',
-               'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
-               'InventoryItem': 'df8539b8-104b-413c-bbf7-651dc857bf50',
-               'Item': 'b6b223a3-cc0e-43d3-8817-3631f5f0e391'
-               }, 'Raspberry Pi 2 Model B': {
-               'Description': 'Raspberry Pi 2 Model B',
-               'Account': 'ea44f579-9548-4954-baf0-48538aceff1e',
-               'TrackingCode': 'd590095a-fe46-4a3e-8989-3c9564d69d4e',
-               'InventoryItem': 'a90f1015-c749-464b-85b6-5bbfa17e3ce8',
-               'Item': '42f5176a-d441-494e-9de5-0372b9304d1b'
-               }
-               }
+acounts = {'SalesInvoiceReceipt': 'd1489e95-bb28-4f5d-b42e-67d3291b3893'}
 
 
 def is_num(ref):
@@ -64,10 +25,17 @@ def get_manager_info(host, user, business):
     static_info = dict()
 
     manager_object = pm.manager_object(host, user, business)
-    customers = manager_object.get_customers()
-    sales_invoices = manager_object.get_sales_invoices()
-    # sales_items = manager_object.get_sales_inventory_items()
-    sales_items = dict()
+
+    sales_items = manager_object.get_inventory()
+    sales_items_new = dict()
+    for item_ref in sales_items:
+        sales_items_new[sales_items[item_ref]['Code']] = {
+            'Id': item_ref,
+            'Description': sales_items[item_ref]['Description'],
+        }
+
+    sales_items = sales_items_new
+    log_file.write('We got sales Items\n')
 
     track_codes = manager_object.get_tracking_codes()
     for code_ref in track_codes:
@@ -95,6 +63,9 @@ def get_manager_info(host, user, business):
     log_file.write('We got success and handling fee: ' +
                    static_info['success_fee'] +
                    ' ' + static_info['handling_fee'] + '\n')
+
+    customers = manager_object.get_customers()
+    sales_invoices = manager_object.get_sales_invoices()
 
     return(manager_object, customers, sales_invoices, sales_items, static_info)
 
@@ -200,6 +171,11 @@ def post_invoice_return(manager_object, customers, invoice, ref):
         return(None, None)
 
 
+def post_receipt_return(manager_object, ref, invoice):
+
+    print(invoice)
+
+
 def try_appends(objs, obj_lst, key):
     for ref in objs:
         try:
@@ -211,10 +187,11 @@ def try_appends(objs, obj_lst, key):
 
 
 def __main__():
+    # xvTcVN8QNxusX3S6xSLd
     user = 'api'
     host = 'https://manager.procsum.co.za'
-    invoice_file = open('invoice.csv', 'r')
-    statement_file = open('statement.csv', 'r')
+    # invoice_file = open('invoice.csv', 'r')
+    # statement_file = open('statement.csv', 'r')
     m_o, customers, invoices, items, static_info = get_manager_info(
        host, user, 'ProcSum')
 
@@ -224,7 +201,7 @@ def __main__():
     try_appends(customers, m_emails, 'Email')
     try_appends(invoices, m_refs, 'Reference')
 
-    invoices = get_data(invoice_file, statement_file, m_refs)
+    # new_invoices = get_data(invoice_file, statement_file, m_refs)
 
     print('Got all the info.\n')
 
@@ -232,10 +209,17 @@ def __main__():
                  for key, value in customers.items()
                  if 'Email' in value}
 
-    for ref in invoices:
-        email, obj = post_customer_return(m_o, invoices[ref], m_emails)
-        customers[email] = obj
-        invoice, obj = post_invoice_return(m_o, customers, invoices[ref], ref)
+    # pprint.pprint(invoices[0])
+    # pprint.pprint(customers)
+    # pprint.pprint(static_info)
+    pprint.pprint(items)
+
+
+    # for ref in new_invoices:
+    #     email, obj = post_customer_return(m_o, new_invoices[ref], m_emails)
+    #     customers[email] = obj
+    #     invoice, obj = post_invoice_return(m_o, customers,
+    #                                        new_invoices[ref], ref)
 
     print('Posted all the things.\n')
 
